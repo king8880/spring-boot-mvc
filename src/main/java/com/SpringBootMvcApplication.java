00f412472569c1541a9cf6cb805fcff4bdfb8858
fahem <kebairf@gmail.com>
2017-05-23 14:16:26 +0100

    chargement des données à partir du main

--- src/main/java/com/SpringBootMvcApplication.java
+++ src/main/java/com/SpringBootMvcApplication.java
@@ -7,29 +7,31 @@ import org.springframework.context.annotation.Bean;
 
 import com.domain.Author;
 import com.domain.Journal;
+import com.repository.AuthorRepository;
 import com.repository.JournalRepository;
 
 @SpringBootApplication
 public class SpringBootMvcApplication {
 	
-//	@Bean
-//	InitializingBean saveData(JournalRepository repo){
-//	return () -> {
-//		Journal j =  new Journal("Get to know Spring Boot","Today I will learn SpringBoot",
-//				"01/01/2016");
-//		Author a = new Author("jim");
-//		a.setJournal(j);
+	@Bean
+	InitializingBean saveData(JournalRepository repo, AuthorRepository aRepo){
+	return () -> {
+		Journal j =  new Journal("Get to know Spring Boot","Today I will learn SpringBoot",
+				"01/01/2016");
+		Author a = new Author("jim");
+		a.getJournaux().add(j);
 //		j.getAuthors().add(a);
 //		repo.save(j);
-//		
+		aRepo.save(a);
+		
 //		j = new Journal("Simple Spring Boot Project","I will do my first SpringBoot Project","01/02/2016");
 //		a = new Author("bill");
 //		a.setJournal(j);
 //		j.getAuthors().add(a);
 //		repo.save(j);
-//
-//		};
-//	}
+
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcApplication.class, args);
